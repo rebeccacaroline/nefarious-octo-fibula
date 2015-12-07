2591ebe93ee22f1ea205d5bda8599795d55bded3
rebeccacaroline <rebeccaholzschuh@gmail.com>
2015-12-07 09:34:08 -0800

    methods for item and outfit - methods for weather_condition and weather_item still to come

--- models/item.rb
+++ models/item.rb
@@ -1,3 +1,19 @@
 class Item
   #this represents a single item in an outfit. sample items include a sweater, a tshirt, a scarf, or gloves
+
+  def is_top?
+  end
+
+  def is_bottom?
+  end
+
+  def is_top_and_bottom?
+    #this is a dress or a romper or footsie pajamas
+  end
+
+  def is_accessory?
+  end
+
+  def is_shoes?
+  end
 end
--- models/outfit.rb
+++ models/outfit.rb
@@ -1,3 +1,19 @@
 class Outfit
   #this will be a collection of items that create one cohesive outfit that a person could wear.
+
+  #This will return an item object where is_outerwear is true
+  def outerwear
+  end
+
+  #this will either return 2 items - a top and a bottom, or 1 top_and_bottom item.
+  def top_and_bottom
+  end
+
+  #this will return an array of accessories
+  def accessories
+  end
+
+  #this will return an item where is_shoe is true
+  def shoes
+  end
 end
